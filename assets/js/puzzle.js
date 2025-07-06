80ad5f6d429661e315b6f24f37f0351c59ec0067
trh4ckn0n <trhacknon@proton.me>
2025-07-06 03:31:00 +0200

    Update puzzle.js

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -1,6 +1,6 @@
 function checkPuzzle() {
     const sequence = prompt("Code d'accès ?");
-    if (sequence === "1337-anon") {
+    if (sequence === "1337-trkn") {
         window.location.href = "/dashboard";
     } else {
         alert("🔒 Code incorrect. Reviens quand t'es prêt.");
